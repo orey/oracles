d0322e547dd64a7f88217667dcec7e1967818c0f
Olivier Rey <rey.olivier@gmail.com>
2020-09-08 22:04:19 +0200

    modif

--- oracles.js
+++ oracles.js
@@ -1,44 +1,36 @@
-'use strict';
+/********************************************
+ * JS utilities
+ * Author: rey.olivier@gmail.com
+ * License: GPL V3
+ * Date: September 08 2020
+ *******************************************/
 
-function getJsonFile(){
-    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-    alert("hola!");
-    fetch(url)
-//    fetch("./names.json")
-        .then(res => {
-            res.text();
-            
-        }
-             )
-        .then(out => {
-            alert('Output: ', out);
-        }
-             )
-        .catch(err => alert(err));
-}
-
-/*window.onload = function() {
-    document.getElementById("button1").addEventListener('click',getJsonFile);
-}*/
-
+/*---------------------------------------
+ * Function to retrieve the base URL
+ * Used for JSON files
+ *--------------------------------------*/
 function getBaseUrl() {
     var re = new RegExp(/^.*\//);
     return re.exec(window.location.href);
 }
 
+/*---------------------------------------
+ * Function to get the JSON file and provide
+ * a JSON object
+ *--------------------------------------*/
+function getJsonFile(url){
+    (async () => {
+        let response = await fetch(url);
+        let commits = await response.json(); // read response body and parse as JSON
+        alert(commits[0].name);
+    })()    
+}
+
 
 function test(){
-    alert("new test");
-
-    (async () => {
-        let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-        let url2 = 'https://raw.githubusercontent.com/orey/oracles/master/names.json';
-        let url3 = './names.json';
-        let response = await fetch(url3);
-
-        let commits = await response.json(); // read response body and parse as JSON
-
-        alert(commits[0].name);
-})()
-    
+    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+    let url2 = 'https://raw.githubusercontent.com/orey/oracles/master/names.json';
+    let url3 = getBaseUrl() + 'names.json';
+    alert(url2);
+    getJsonFile(url2);
 }
