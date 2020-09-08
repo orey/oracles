854f546dbb27549d7d434a749f687373e1e60a0d
Olivier Rey <rey.olivier@gmail.com>
2020-09-08 06:35:57 +0200

    initial commit

--- oracles.js
+++ oracles.js
@@ -1,8 +1,17 @@
 var getJsonFile(){
-    fetch('names.json')
-        .then(res => res.json())
+    alert("hola!");
+    fetch('https://raw.githubusercontent.com/orey/oracles/master/names.json')
+        .then(res => {
+            res.json();
+            alert(res);
+        }
+        )
         .then((out) => {
             alert('Output: ', out);
         }).catch(err => alert(err));
 }
+
+/*window.onload = function() {
+    document.getElementById("button1").addEventListener('click',getJsonFile);
+}*/
 
