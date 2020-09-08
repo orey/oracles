6b5f0dbc1c4e8da19d61a1ddfcdb3b210e37cd8f
Olivier Rey <rey.olivier@gmail.com>
2020-09-08 16:41:50 +0200

    adding js

--- oracles.js
+++ oracles.js
@@ -21,6 +21,11 @@ function getJsonFile(){
     document.getElementById("button1").addEventListener('click',getJsonFile);
 }*/
 
+function getBaseUrl() {
+    var re = new RegExp(/^.*\//);
+    return re.exec(window.location.href);
+}
+
 
 function test(){
     alert("new test");
@@ -28,7 +33,8 @@ function test(){
     (async () => {
         let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
         let url2 = 'https://raw.githubusercontent.com/orey/oracles/master/names.json';
-        let response = await fetch(url2);
+        let url3 = './names.json';
+        let response = await fetch(url3);
 
         let commits = await response.json(); // read response body and parse as JSON
 
