7d14fbb419bccf11b42c70808e6a3a6e0735f09e
Olivier Rey <rey.olivier@gmail.com>
2020-09-08 16:38:34 +0200

    modified

--- oracles.js
+++ oracles.js
@@ -1,17 +1,38 @@
-var getJsonFile(){
+'use strict';
+
+function getJsonFile(){
+    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
     alert("hola!");
-    fetch('https://raw.githubusercontent.com/orey/oracles/master/names.json')
+    fetch(url)
+//    fetch("./names.json")
         .then(res => {
-            res.json();
-            alert(res);
+            res.text();
+            
         }
-        )
-        .then((out) => {
+             )
+        .then(out => {
             alert('Output: ', out);
-        }).catch(err => alert(err));
+        }
+             )
+        .catch(err => alert(err));
 }
 
 /*window.onload = function() {
     document.getElementById("button1").addEventListener('click',getJsonFile);
 }*/
 
+
+function test(){
+    alert("new test");
+
+    (async () => {
+        let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+        let url2 = 'https://raw.githubusercontent.com/orey/oracles/master/names.json';
+        let response = await fetch(url2);
+
+        let commits = await response.json(); // read response body and parse as JSON
+
+        alert(commits[0].name);
+})()
+    
+}
