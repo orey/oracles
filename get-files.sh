57b2a2e6aae0cd73cbf980c4ef5aaa6102f7de02
orey <rey.olivier@gmail.com>
2020-10-10 15:42:54 +0200

    new version

---
+++ get-files.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git checkout master -- index.html
+git checkout master -- oracles.js
+git checkout master -- js/oracles-engine.js
+git checkout master -- js/names.js
+git checkout master -- js/harrypotter.js
+
--- oracles.js
+++ oracles.js
@@ -4,6 +4,35 @@
  * License: GPL V3
  * Date: September 08 2020
  *******************************************/
+"use strict";
+
+const BASE_URL = 'https://raw.githubusercontent.com/orey/oracles/master/';
+
+let 
+
+function myTrace(text) {
+    let elem = document.getElementById('trace');
+    elem.innerHTML = "<h3>Trace</h3><p>" + text + "</p>";
+}
+
+
+/*---------------------------------------
+ * Function to load other JS files
+ *--------------------------------------*/
+function loadJS(url) {
+    let scriptTag = document.createElement('script');
+    scriptTag.src = url;
+    document.body.appendChild(scriptTag);
+}
+
+function loadDependencies() {
+    loadJS(BASE_URL + "js/oracles-engine.js");
+    myTrace(BASE_URL + "js/oracles-engine.js");
+    myTrace("D20:" + testRollDie(20).toString())
+    //hp     = loadJS(BASE_URL + "js/harrypotter.js");
+    //names  = loadJS(BASE_URL + "js/names.js");
+}
+
 
 /*---------------------------------------
  * Function to retrieve the base URL
@@ -22,15 +51,20 @@ function getJsonFile(url){
     (async () => {
         let response = await fetch(url);
         let commits = await response.json(); // read response body and parse as JSON
-        alert(commits[0].name);
+        myTrace(commits[0].name);
     })()    
 }
 
 
 function test(){
-    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-    let url2 = 'https://raw.githubusercontent.com/orey/oracles/master/names.json';
-    let url3 = getBaseUrl() + 'names.json';
+    //loadDependencies();
+    let url2 = BASE_URL + 'names.json';
     alert(url2);
+    // let url3 = getBaseUrl() + 'names.json';
+    myTrace(url2);
     getJsonFile(url2);
+    myTrace("D20:" + testRollDie(20).toString())
+    //let NPC = new NPC();
+    //alert("NPC name: " + NPC.name + " " + NPC.surname);
+    
 }
