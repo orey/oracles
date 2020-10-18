5fa8504e9313ec7d3d42e3a7c04eaff4398fcf51
orey <rey.olivier@gmail.com>
2020-10-18 17:15:03 +0200

    new stuff

--- npccomponent.js
+++ npccomponent.js
@@ -8,61 +8,186 @@
  *******************************************/
 "use strict";
 
-class NpcView extends HTMLElement {
+const DEFAULT_SHADOW = "<p>Nothing</p>";
+
+
+/*-------------------------------------------------------
+ * This class is the basic event manager that enables web
+ * components to communicate together.
+ *-------------------------------------------------------*/
+class EventBus {
     constructor() {
+        this._bus = document.createElement('div');
+    }
+    
+    register(event, callback) {
+        this._bus.addEventListener(event, callback);
+    } 
+    
+    remove(event, callback) {
+        this._bus.removeEventListener(event, callback);
+    }
+
+    fire(event, detail = {}) {
+        this._bus.dispatchEvent(new CustomEvent(event, { detail }));
+    }
+}
+
+// BUS will be the web components event manager
+const BUS = new EventBus();
+
+
+/*-------------------------------------------------------
+ * This class is the root class of all our web components. It defined the data bindings
+ * and reusable mechanisms of the shadow DOM.
+ * MyHTMLElements is a View.
+ *-------------------------------------------------------*/
+class MyHTMLElement extends HTMLElement {
+    constructor(){
         super();
-        // element created
-        this._npc = undefined;
+        this._data = undefined;
+        this._shadow = this.attachShadow({mode: 'open'});
         this._rendered = false;
-        this.shadow = this.attachShadow({mode: 'open'});
     }
 
     connectedCallback() {
         this.render();
     }
 
-    get npc() {
-        return this._npc;
+    /*
+     * Data is the model, a MyHTMLElement being a view. The vioew points to the model.
+     * data is a javascript object.
+     */
+    set data(dat){
+        if ((dat == undefined) || (dat == {})) {
+            this._data = undefined;
+            this._rendered = false;
+            return;
+        }
+        this._data = dat;
+        this._rendered = false;
     }
 
-    set npc(anpc) {
-        this._npc = anpc;
+    get data(){
+        return this._data;
     }
 
-    // This method was refreshing the component by changing an attribute
-    // It works but the event bus seems better
-    
-    /*  static get observedAttributes() {
-        return ['render'];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        console.log("Attribute " + name + " changed.");
-        this.render();
-    }*/
-
+    /*
+     * Method to overload
+     */
     render(){
-        if (this._npc == undefined)
+        this._shadow.innerHTML = DEFAULT_SHADOW;
+    }
+
+}
+
+
+/*-------------------------------------------------------
+ * This class is a View for NPC class
+ *-------------------------------------------------------*/
+class NpcView extends MyHTMLElement {
+    render(){
+        if (this._data == undefined)
+        {
+            console.log("No data in view");
             // do nothing
             return;
+        }
         if (this._rendered) {
-            // remove
-            this.shadow.innerHTML = "<p>Removed!</p>"
-            this._rendered = false;
+            console.log("Already rendered");
+            return;
         }
-        else {
-            // add
-            this.shadow.innerHTML = this._npc.to_HTML();
-            this._rendered = true;
-        }
+        this._shadow.innerHTML = this._data.to_HTML();
+        this._rendered = true;
     }
     
 }
+
 // let the browser know that <my-npc> is served by our new class
 customElements.define("my-npc", NpcView);
 
 BUS.register("my-npc-refresh", (evt) => {
     let e = document.querySelector("my-npc");
+    e.render();
+});
+
+/*-------------------------------------------------------
+ * This class is a View for NPC class
+ *-------------------------------------------------------*/
+const TMPL_M1 =`
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+  /* height: 300px; /* Should be removed. Only for demonstration */*/
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+<h3>`;
+const TMPL_M2 = `
+</h3>
+<div class="row">
+  <div class="column">`;
+const TMPL_M3 = `</div>
+  <div class="column">`;
+const TMPL_M4 = `</div></div>`;
+
+const TMPL_T1 = `<table>
+              <tr>
+                <th>Caractéristiques</th>
+                <th>Valeur</th>
+              </tr>`;
+const TMPL_T2 = "</table>";
+
+
+
+class NpcView2 extends MyHTMLElement {
+    render(){
+        if (this._data == undefined)
+        {
+            console.log("No data in view");
+            // do nothing
+            return;
+        }
+        if (this._rendered) {
+            console.log("Already rendered");
+            return;
+        }
+
+        // parsing this._data;
+        let fragment = TMPL_M1 + "Nom : " + this._data.name + " " + this._data.surname + TMPL_M2;
+        fragment += TMPL_T1;
+        for (var c in this._data.traits)
+            fragment += "<tr><td>" + c + "</td><td>" + this._data.traits[c] + "</td></tr>\n";
+        fragment += TMPL_T2 + TMPL_M3 + TMPL_T1;
+        for (var c in this._data.others)
+            fragment += "<tr><td>" + c + "</td><td>" + this._data.others[c] + "</td></tr>\n";
+        fragment += TMPL_T2 + TMPL_M4;
+      
+        //this._shadow.innerHTML = this._data.to_HTML();
+        this._shadow.innerHTML = fragment;
+        this._rendered = true;
+    }
+    
+}
+
+// let the browser know that <my-npc> is served by our new class
+customElements.define("my-npc2", NpcView2);
+
+BUS.register("my-npc2-refresh", (evt) => {
+    let e = document.querySelector("my-npc2");
     e.render();
 });
 
